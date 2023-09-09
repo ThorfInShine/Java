1ecbcff9fb606cc94e1f0409d6e88e58bd2a4b74
ThorfInShine <135157777+ThorfInShine@users.noreply.github.com>
2023-09-09 22:34:10 +0700

    Delete latihan1 directory

--- latihan1/apaaja.java
+++
@@ -1,6 +0,0 @@
-package latihan1;
-public class apaaja {
-    public static void main(String[] args) {
-        System.out.println("Ya terserah sih");
-    }
-}
--- latihan1/latihan.java
+++
@@ -1,6 +0,0 @@
-package latihan1;
-public class latihan {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
