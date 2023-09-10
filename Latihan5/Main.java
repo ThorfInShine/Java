6942188752ecbb3a327d317150e1c5c66ad50ea3
ThorfInShine <135157777+ThorfInShine@users.noreply.github.com>
2023-09-10 11:01:49 +0700

    Add files via upload

---
+++ Latihan5/Main.java
@@ -0,0 +1,76 @@
+package Latihan5;
+/*  TIPE DATA YANG ADA DI JAVA :
+        INTEGER, BYTE, SHORT, LONG, DOUBLE, FLOAT, CHAR, BOOLEAN */
+public class Main {
+    public static void main(String[] args) {
+       /* INTEGER (SATUAN) */
+        int i = 10;
+       System.out.println("=========INTEGER========");
+       System.out.println("NILA INTEGER I = " + i);
+       System.out.println("NILAI MAX = " + Integer.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Integer.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Integer.BYTES);
+       System.out.println("BESAR INTEGER = " + Integer.SIZE);
+
+       /* BYTE (SATUAN) */
+        byte b = 15;
+       System.out.println("=========BYTE========");
+       System.out.println("NILA BYTE B = " + b);
+       System.out.println("NILAI MAX = " + Byte.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Byte.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Byte.BYTES);
+       System.out.println("BESAR INTEGER = " + Byte.SIZE);
+       
+       /* SHORT (SATUAN)*/
+        short s = 20;
+       System.out.println("=========SHORT========");
+       System.out.println("NILA SHORT S = " + s);
+       System.out.println("NILAI MAX = " + Short.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Short.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Short.BYTES);
+       System.out.println("BESAR INTEGER = " + Short.SIZE);
+
+       /* LONG (SATUAN)*/
+        long l = 25L;
+       System.out.println("=========LONG========");
+       System.out.println("NILA LONG L = " + l);
+       System.out.println("NILAI MAX = " + Long.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Long.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Long.BYTES);
+       System.out.println("BESAR INTEGER = " + Long.SIZE);
+
+       /* DOUBLE (KOMA, BILANGAN REAL)*/
+        double d = 3.0d;
+       System.out.println("=========DOUBLE========");
+       System.out.println("NILA DOUBLE D = " + d);
+       System.out.println("NILAI MAX = " + Double.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Double.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Double.BYTES);
+       System.out.println("BESAR INTEGER = " + Double.SIZE);
+
+       /* FLOAT (KOMA, BILANGAN REAL)*/
+        float f = 3.5f;
+       System.out.println("=========FLOAT========");
+       System.out.println("NILA FLOAT F = " + f);
+       System.out.println("NILAI MAX = " + Float.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Float.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Float.BYTES);
+       System.out.println("BESAR INTEGER = " + Float.SIZE);
+
+       /* CHAR  (KARAKTER) BERDASARKAN ASCII*/
+        char c = 'C';
+       System.out.println("=========CHAR========");
+       System.out.println("NILA CHAR C = " + 'C');
+       System.out.println("NILAI MAX = " + Character.MAX_VALUE);
+       System.out.println("NILAI MIN = " + Character.MIN_VALUE);
+       System.out.println("BESAR INTEGER = " + Character.BYTES);
+       System.out.println("BESAR INTEGER = " + Character.SIZE);
+       
+       /* BOOLEAN (NILAI TRUE ATAU FALSE)*/
+        boolean bol = true;
+       System.out.println("=========BOOLEAN========");
+       System.out.println("NILA BOOOLEAN bol = " + bol);
+       System.out.println("NILAI MAX = " + Boolean.TRUE);
+       System.out.println("NILAI MIN = " + Boolean.FALSE);
+    }
+}
