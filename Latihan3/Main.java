7cbfbea4dc9234f7c7a62c83ac4a376c4863130d
ThorfInShine <135157777+ThorfInShine@users.noreply.github.com>
2023-09-10 08:27:15 +0700

    Add files via upload

---
+++ Latihan3/Main.java
@@ -0,0 +1,21 @@
+package Latihan3;
+
+/* MEMAHAMI STRING ARGS */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Halo " + args[0] + " ganteng");
+        System.out.println("Seperti biasa " + args[1] + " memang keren");
+        System.out.println("Gitu aja sih wir");
+    }
+}
+/* CARA MENJALANKAN KODE DIATAS
+1. COMPILE FILE MAIN.JAVA TERLEBIH DAHULU (javac Latihan3\Main.java)
+2. SETELAH DI COMPILE JALAN PERINTAH java Latihan3.Main Thareeq Kamu (opsional)
+3. RUN KODE SEPERTI BIASA
+
+KETERANGAN TAMBAHAN:
+Latihan3                        = nama package (letak file yang ingin dicompile)
+Main.java                       = nama file yang ingin di compile
+Thareeq (args [0]) (opsional)   = ini adalah nilai args[0] bisa diisi terserah mau apa aja
+Kamu (args[1]) (opsional)       =  ini adalah nilai args[0] bisa diisi terserah mau apa aja
+Latihan3\Main.java              = ini adalah package/folder dimana Main.java disimpan */
