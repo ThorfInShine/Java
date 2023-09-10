a67f8adea71e6ab027286550058acaffabfcdb8d
ThorfInShine <135157777+ThorfInShine@users.noreply.github.com>
2023-09-10 07:23:48 +0700

    Add files via upload

---
+++ Latihan2/Main.java
@@ -0,0 +1,20 @@
+package Latihan2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Baris Ke-2 : Hai juga woy");
+
+        System.out.print("Baris Ke-3 : Apaasn sih lu");
+        System.out.print("Baris Ke-4 : Terserah gwe lah");
+
+        System.out.print("Baris Ke-5 : Halo cuy");
+        System.out.println("Baris Ke-6 : Halo juga wir");
+
+        System.out.print("Baris Ke-6 : Halo semuanya\n");
+        System.out.print("Baris Ke-7 : Yaudah sih wir\n");
+        System.out.print("Baris Ke-8 : Sok asik lu wir\n");
+
+        System.out.printf("Dewa penghancur yang lahir pada abad ke %d", 9);
+    }
+}
