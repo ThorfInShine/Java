cb20aa37f243faba250055527083bb463bbb473c
ThorfInShine <135157777+ThorfInShine@users.noreply.github.com>
2023-09-10 09:27:38 +0700

    Add files via upload

---
+++ Latihan4/Main.java
@@ -0,0 +1,22 @@
+package Latihan4;
+
+/* Variabel, Assignment dan Deklarasi */
+
+public class Main {
+    public static void main(String[] args) {
+        /* Variabel */
+        int a=10;
+        System.out.println("nilai a = " + a);
+        /* Kita juga dapat mengubah nilai variabel yang sudah di print dengan cara */
+        a=20;
+        System.out.println("nilai a baru = " + a);
+
+        /* Deklarasi */
+        int b;
+        b=10;
+        System.out.println("nilai b = " + b);
+        /* Kita juga dapat mengubah nilai deklarasi yang sudah di print dengan cara */
+        b=15;
+        System.out.println("nilai b baru = " + b);
+    }
+}
